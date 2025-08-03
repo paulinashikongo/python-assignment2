8b638e3c29013c6516819ec81502c72add35fc04
paulinashikongo <paulinashikongo77.ps@gmail.com>
2025-08-03 11:37:50 +0200

    Update Assignmenttwo.py

--- Assignmenttwo.py
+++ Assignmenttwo.py
@@ -17,3 +17,7 @@ index_of_30 = my_list.index(30)
 
 print("Final list:", my_list)
 print("Index of 30:", index_of_30)
+PS C:\Users\user\Desktop\PLP Codes\PLP_Python>  & 'c:\Users\user\AppData\Local\Programs\Python\Python313\python.exe' 'c:\Users\user\.vscode\extensions\ms-python.debugpy-2025.10.0-win32-x64\bundled\libs\debugpy\launcher' '50481' '--' 'c:\Users\user\Desktop\PLP Codes\PLP_Python\Assignmenttwo.py' 
+Final list: [10, 15, 20, 30, 40, 50, 60]
+Index of 30: 3
+
